debf6e6e2de6c3a60e8336348ec5d9de50ae2ead
stefan.friesen <stefan.friesen@informatik.hs-fulda.de>
2023-05-10 15:32:02 +0200

    first layout

---
+++ FPRG_EmojiRain/Assets/Scripts/WebcamTexture.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebcamTexture : MonoBehaviour
+{
+    [SerializeField] RawImage webcamRawImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamT = new WebCamTexture();
+        webcamRawImage.texture = webcamT;
+        webcamRawImage.material.mainTexture = webcamT;
+        webcamT.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
