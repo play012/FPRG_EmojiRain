d814d9440bc8d84959f04c9a59e1916d1b38cd81
stefan.friesen <stefan.friesen@informatik.hs-fulda.de>
2023-05-29 18:12:31 +0200

    Gameplay loop

---
+++ FPRG_EmojiRain/Assets/Scripts/CloudManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] emojis;
+    [SerializeField] GameObject canvasGO;
+    [SerializeField] CollisionManager collManager;
+
+    Vector3 dir;
+    float speed;
+    bool emojisSpawned;
+
+    void Start() {
+        dir = Vector3.left;
+        speed = 3f;
+        emojisSpawned = true;
+    }
+
+    void Update() {
+        if(collManager.hitCounter != -1) {
+            transform.Translate(dir*speed*Time.deltaTime);
+
+            if(transform.position.x < -5.75f) {
+                dir = Vector3.right;
+                speed = Random.Range(3f, 10f);
+                emojisSpawned = false;
+            } else if(transform.position.x > 5.75f) {
+                dir = Vector3.left;
+                speed = Random.Range(3f, 10f);
+                emojisSpawned = true;
+            }
+
+            if(transform.position.x >= 0.001f && !emojisSpawned) {
+                emojisSpawned = true;
+                //int emojiCount = Random.Range(1, 4);
+                int emojiCount = 1;
+                for(int i = 0; i < emojiCount; i++) {
+                    int randEmoji = Random.Range(0, emojis.Length);
+                    float randPos = Random.Range(-250f, 250f);
+                    GameObject emojiInst = Instantiate(emojis[randEmoji], new Vector3(randPos, 280f, 0f), Quaternion.identity);
+                    emojiInst.transform.SetParent(canvasGO.transform, false);
+                }
+            }
+        }
+    }
+}
---
+++ FPRG_EmojiRain/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionManager : MonoBehaviour
+{
+    public int hitCounter;
+
+    [SerializeField] Texture emojiTexture;
+    [SerializeField] GameObject[] hearts;
+    [SerializeField] GameObject cloudGO, buttonGO;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Emoji") {
+            RawImage emojiImg = other.transform.GetChild(0).gameObject.GetComponent<RawImage>();
+            emojiImg.texture = emojiTexture;
+            hearts[hitCounter].SetActive(false);
+            hitCounter--;
+
+            if (hitCounter == -1) {
+                cloudGO.transform.position = new Vector3(0f, 3f, -0.1f);
+                buttonGO.SetActive(true);
+                foreach (GameObject heartGO in hearts) {
+                    heartGO.SetActive(true);
+                }
+
+                var clones = GameObject.FindGameObjectsWithTag("Emoji");
+                foreach (var clone in clones) {
+                    Destroy(clone);
+                }
+            }
+        }
+    }
+
+    public void ResetHits() {
+        hitCounter = hearts.Length - 1;
+    }
+
+    void Start() {
+        ResetHits();
+    }
+}
