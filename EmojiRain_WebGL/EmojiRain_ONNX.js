0e95fff3306c48b44e0778410a25ad10b9635059
stefan.friesen <stefan.friesen@informatik.hs-fulda.de>
2023-06-14 14:54:23 +0200

    add testing for multiple ONNX models

--- EmojiRain_WebGL/EmojiRain_ONNX.js
+++ EmojiRain_WebGL/EmojiRain_ONNX.js
@@ -1,7 +1,9 @@
 const sess = new onnx.InferenceSession();
-sess.loadModel("./EmojiRain.onnx");
+sess.loadModel("./emotion-ferplus-2.onnx");
 
 async function processVideoFrames() {
+  localStorage.setItem('emotion', '');
+
   //Webcam stream
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   const videoElement = document.createElement('video');
@@ -11,36 +13,75 @@ async function processVideoFrames() {
   const context = canvas.getContext('2d');
   const inputSize = 64;
   const inputTensorSize = 1 * 1 * inputSize * inputSize;
-  canvas.width = inputSize;
-  canvas.height = inputSize;
 
-  function processFrame() {
-    context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+  async function processFrame() {
+    const originalWidth = videoElement.videoWidth;
+    const originalHeight = videoElement.videoHeight;
+    const targetWidth = 64;
+    const targetHeight = 64;
 
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const sourceAspectRatio = originalWidth / originalHeight;
+    const targetAspectRatio = targetWidth / targetHeight;
+
+    let scaledWidth, scaledHeight;
+    if (sourceAspectRatio > targetAspectRatio) {
+      scaledWidth = targetWidth;
+      scaledHeight = Math.floor(targetWidth / sourceAspectRatio);
+    } else {
+      scaledWidth = Math.floor(targetHeight * sourceAspectRatio);
+      scaledHeight = targetHeight;
+    }
+
+    const xOffset = Math.floor((targetWidth - scaledWidth) / 2);
+    const yOffset = Math.floor((targetHeight - scaledHeight) / 2);
+
+    context.drawImage(videoElement, 0, 0, originalWidth, originalHeight, xOffset, yOffset, scaledWidth, scaledHeight);
+
+    const imageData = context.getImageData(xOffset, yOffset, scaledWidth, scaledHeight);
     const inputTensorData = new Float32Array(inputTensorSize);
 
-    //Preprocessing
+    // Preprocessing
     const inputChannelData = imageData.data;
     for (let i = 0; i < inputSize; i++) {
       for (let j = 0; j < inputSize; j++) {
         const inputIndex = (0 * inputSize + i) * inputSize + j;
         const inputPixelIndex = (i * inputSize + j) * 4;
-        const pixelValue = (inputChannelData[inputPixelIndex] + inputChannelData[inputPixelIndex + 1] + inputChannelData[inputPixelIndex + 2]) / 3 / 255;
-        inputTensorData[inputIndex] = pixelValue;
+        const pixelValue = (inputChannelData[inputPixelIndex] + inputChannelData[inputPixelIndex + 1] + inputChannelData[inputPixelIndex + 2]) / 3;
+        const normalizedPixelValue = (pixelValue - 127.5) / 127.5; // Normalize to the range [-1, 1]
+        inputTensorData[inputIndex] = normalizedPixelValue;
       }
     }
 
     const inputTensor = new onnx.Tensor(inputTensorData, 'float32', [1, 1, inputSize, inputSize]);
 
-    const outputMap = sess.run([inputTensor]);
-    const outputTensor = outputMap.values().next().value;
-    const outputData = outputTensor.data;
+    const outputMap = await sess.run([inputTensor]);
+    console.log(outputMap.get('Plus692_Output_0').internalTensor.data);
+
+    function softmax(arr) {
+      const max = Math.max(...arr);
+      const expArr = arr.map(x => Math.exp(x - max));
+      const sumExp = expArr.reduce((sum, val) => sum + val, 0);
+      return expArr.map(x => x / sumExp);
+    }
+    
+    const outputArray = Array.from(outputMap.get('Plus692_Output_0').internalTensor.data);
+    const softmaxValues = softmax(outputArray);
+    console.log(softmaxValues);
+
+    const highestEmotionValue = 0;
+
+    outputMap.get('Plus692_Output_0').internalTensor.data.forEach(emotionValue => {
+      Object.keys(emotionValue).forEach(emotionKey => {
+        if (emotionValue[emotionKey] > highestEmotionValue) {
+          highestEmotionValue = emotionValue[emotionKey];
+        }
+      });
+    });
 
     const emotionLabels = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt'];
-    const scores = Array.from(outputData);
-    const maxScoreIndex = scores.indexOf(Math.max(...scores));
-    const predictedEmotion = emotionLabels[maxScoreIndex];
+    const predictedEmotion = emotionLabels[highestEmotionValue];
+
+    console.log(predictedEmotion);
 
     switch (predictedEmotion) {
       case 'happiness':
@@ -69,7 +110,9 @@ async function processVideoFrames() {
 
     requestAnimationFrame(processFrame);
   }
-  requestAnimationFrame(processFrame);
+  videoElement.addEventListener('loadedmetadata', () => {
+    requestAnimationFrame(processFrame); 
+  });
 }
 
 processVideoFrames().catch(error => {
